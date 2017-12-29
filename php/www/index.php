6041da5a8ff4a72f1ac2e0c89b50c3c7c480c26e
root <root@ip-172-31-42-71.us-east-2.compute.internal>
2017-12-29 10:34:38 +0000

    first commit

---
+++ php/www/index.php
@@ -0,0 +1,9 @@
+<?php
+
+mysqli_connect("db", "root", "hunter2") or die(mysqli_error());
+echo "#############################";
+#echo "#                           #";
+echo "# Connected to MySQL-Server #";
+#echo "#                           #";
+echo "#############################";
+?>
